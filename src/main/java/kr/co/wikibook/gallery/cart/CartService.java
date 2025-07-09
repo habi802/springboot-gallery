df3cedc520c212fcd21e28d20684bf628cb1e622
habi802 <shb8002@gmail.com>
2025-07-09 17:09:20 +0900

    [feat] 장바구니 조회 추가

--- src/main/java/kr/co/wikibook/gallery/cart/CartService.java
+++ src/main/java/kr/co/wikibook/gallery/cart/CartService.java
@@ -1,8 +1,11 @@
 package kr.co.wikibook.gallery.cart;
 
+import kr.co.wikibook.gallery.cart.model.CartGetRes;
 import kr.co.wikibook.gallery.cart.model.CartPostReq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +14,9 @@ public class CartService {
 
     public int save(CartPostReq req) {
         return cartMapper.save(req);
+    }
+
+    public List<CartGetRes> findAll(int memberId) {
+        return cartMapper.findAllWithItemByMemberId(memberId);
     }
 }
